de4ca830b3a2bde2b59f9724f0bd6868bf51bb45
ChangUk Park <ukletter@gmail.com>
2015-11-07 18:02:44 +0900

    Init commit

---
+++ NetworkVisualizer/JSONBuilder.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace NetworkVisualizer {
+    public class JSONBuilder {
+        public static void Main(string[] args) {
+            string dirData = @args[0] + Path.DirectorySeparatorChar;           // Path of directory that containes SQLite DB files
+            string curDir = System.Environment.CurrentDirectory;
+
+            string[] sqliteDBs = Directory.GetFiles(dirData, "*.sqlite");
+            foreach (string dbFile in sqliteDBs) {
+                string egoUser = Path.GetFileNameWithoutExtension(dbFile);
+                Console.WriteLine(egoUser);
+
+                try {
+                    // To handle the exception while the program runs
+                    if (!File.Exists(dbFile))
+                        throw new FileNotFoundException(dbFile);
+
+                    // Get DB connection
+                    SQLiteAdapter dbAdapter = new SQLiteAdapter(dbFile);
+
+                    // Get nodes
+                    int nNodes = 0;
+                    Dictionary<string, int> users = new Dictionary<string, int>();
+                    users.Add(egoUser, nNodes++);
+                    HashSet<string> followingUsers = dbAdapter.getFollowingUsers(egoUser);
+                    foreach (string followingUser in followingUsers) {
+                        HashSet<string> followingUsersOfFollowingUser = dbAdapter.getFollowingUsers(followingUser);
+                        if (followingUsersOfFollowingUser.Contains(egoUser))
+                            users.Add(followingUser, nNodes++);
+                    }
+
+                    // Make users' index map to find user id by index
+                    Dictionary<int, string> indexMap = new Dictionary<int, string>();
+                    foreach (string userId in users.Keys)
+                        indexMap.Add(users[userId], userId);
+
+                    // Get edges (undirected)
+                    HashSet<string> visited = new HashSet<string>();
+                    Dictionary<string, HashSet<string>> undirectedEdges = new Dictionary<string, HashSet<string>>();
+                    Dictionary<string, int> linkCount = new Dictionary<string, int>();
+                    foreach (string source in users.Keys) {
+                        HashSet<string> allTargets = dbAdapter.getFollowingUsers(source);
+                        HashSet<string> targets = new HashSet<string>();
+                        foreach (string target in allTargets) {
+                            if (!users.ContainsKey(target))
+                                continue;
+                            if (visited.Contains(target))
+                                continue;
+
+                            targets.Add(target);
+
+                            // Count the number of links of the node
+                            if (!linkCount.ContainsKey(source))
+                                linkCount.Add(source, 0);
+                            linkCount[source] += 1;
+                            if (!linkCount.ContainsKey(target))
+                                linkCount.Add(target, 0);
+                            linkCount[target] += 1;
+                        }
+
+                        undirectedEdges.Add(source, targets);   // Save edges from source node(user)
+                        visited.Add(source);                    // Set visited node
+                    }
+
+                    // Make *.pairs files
+                    string dirFastModularity = curDir + Path.DirectorySeparatorChar + "FastModularity" + Path.DirectorySeparatorChar;
+                    string dirByProducts = dirFastModularity + "clusters" + Path.DirectorySeparatorChar;
+                    if (!Directory.Exists(dirByProducts))
+                        Directory.CreateDirectory(dirByProducts);
+                    string file_pairs = dirFastModularity + egoUser + ".pairs";         // *.pairs
+                    StreamWriter pairsFileMaker = new StreamWriter(file_pairs);
+                    foreach (string source in undirectedEdges.Keys) {
+                        foreach (string target in undirectedEdges[source])
+                            pairsFileMaker.Write(users[source] + "\t" + users[target] + "\n");
+                    }
+                    pairsFileMaker.Close();
+
+                    // Run graph clustering using external c++ program
+                    ProcessStartInfo fastModularity = new ProcessStartInfo();
+                    fastModularity.FileName = dirFastModularity + "FastModularity.exe";
+                    fastModularity.Arguments = "-f \"" + file_pairs + "\"";
+                    fastModularity.WindowStyle = ProcessWindowStyle.Hidden;
+                    fastModularity.CreateNoWindow = true;
+                    using (Process proc = Process.Start(fastModularity)) {
+                        proc.WaitForExit();
+                    }
+
+                    // Load cluster information from a by-product file which is produced during the graph clustering
+                    Dictionary<string, int> groupNumbers = new Dictionary<string, int>();
+                    int groupNumber = 1;
+                    StreamReader reader = new StreamReader(dirByProducts + egoUser);
+                    string line;
+                    while ((line = reader.ReadLine()) != null) {
+                        string[] userIndices = line.Split('\t');
+                        foreach (string indUser in userIndices) {
+                            if (indUser.Length == 0)
+                                continue;
+
+                            int userIndex = int.Parse(indUser);
+                            if (userIndex == 0) {
+                                groupNumbers.Add(egoUser, 0);
+                            } else {
+                                string userId = indexMap[userIndex];
+                                groupNumbers.Add(userId, groupNumber);
+                            }
+                        }
+                        groupNumber += 1;
+                    }
+                    reader.Close();
+
+                    // Write node information
+                    StreamWriter writer = new StreamWriter(dirData + egoUser + ".json");    // Output file
+                    writer.WriteLine("{\r\n\t\"nodes\": [");
+                    bool isFirstRecord = true;
+                    foreach (string userId in users.Keys) {
+                        int cluster = groupNumbers[userId];
+                        if (linkCount[userId] == 1)     // Set the group number of leaf node as -1
+                            cluster = -1;
+
+                        if (isFirstRecord) {
+                            writer.Write("\t\t{\"name\":\"" + userId + "\",\"group\":" + cluster + "}");
+                            isFirstRecord = false;
+                        } else {
+                            writer.Write(",\r\n\t\t{\"name\":\"" + userId + "\",\"group\":" + cluster + "}");
+                        }
+                    }
+
+                    // Write link information
+                    writer.WriteLine("\r\n\t], \"links\": [");
+                    isFirstRecord = true;
+                    foreach (string source in undirectedEdges.Keys) {
+                        foreach (string target in undirectedEdges[source]) {
+                            int mentionCount = (int)dbAdapter.getMentionCount(source, target);
+                            if (mentionCount > 0)
+                                mentionCount = (int)Math.Log(mentionCount);
+                            mentionCount += 1;
+                            if (isFirstRecord) {
+                                writer.Write("\t\t{\"source\":" + users[source] + ",\"target\":" + users[target] + ",\"value\":" + mentionCount + "}");
+                                isFirstRecord = false;
+                            } else {
+                                writer.Write(",\r\n\t\t{\"source\":" + users[source] + ",\"target\":" + users[target] + ",\"value\":" + mentionCount + "}");
+                            }
+                        }
+                    }
+                    writer.WriteLine("\r\n\t]\r\n}");
+
+                    writer.Close();
+                    dbAdapter.closeDB();
+
+                    // Delete the by-product files
+                    File.Delete(file_pairs);
+                    File.Delete(dirByProducts + egoUser);
+                } catch (FileNotFoundException e) {
+                    Console.WriteLine(e);
+                } catch (Exception e) {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
---
+++ NetworkVisualizer/SQLiteAdapter.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace NetworkVisualizer {
+    public class SQLiteAdapter {
+        private SQLiteConnection conn = null;
+
+        public SQLiteAdapter(string dbPath) {
+            try {
+                SQLiteConnectionStringBuilder connBuilder = new SQLiteConnectionStringBuilder();
+                connBuilder.DataSource = dbPath;
+                connBuilder.Version = 3;
+                connBuilder.JournalMode = SQLiteJournalModeEnum.Wal;
+                this.conn = new SQLiteConnection(connBuilder.ToString());
+                this.conn.Open();
+            } catch (Exception e) {
+                throw e;
+            }
+        }
+
+        public void closeDB() {
+            if (conn != null)
+                conn.Close();
+        }
+
+        public HashSet<string> getFollowingUsers(string userId) {
+            HashSet<string> userList = new HashSet<string>();
+            using (SQLiteCommand cmd = new SQLiteCommand(conn)) {
+                cmd.CommandText = "SELECT target FROM follow WHERE source = " + userId;
+                using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                        string followee = reader.GetValue(0).ToString();
+                        userList.Add(followee);
+                    }
+                }
+            }
+            return userList;
+        }
+
+        public HashSet<long> getAuthorship(long userId) {
+            HashSet<long> tweetList = new HashSet<long>();
+            using (SQLiteCommand cmd = new SQLiteCommand(conn)) {
+                cmd.CommandText = "SELECT id FROM tweet WHERE author = " + userId;
+                using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                        long tweet = (long)reader.GetValue(0);
+                        tweetList.Add(tweet);
+                    }
+                }
+            }
+            return tweetList;
+        }
+
+        public HashSet<long> getRetweets(long userId) {
+            HashSet<long> tweetList = new HashSet<long>();
+            using (SQLiteCommand cmd = new SQLiteCommand(conn)) {
+                cmd.CommandText = "SELECT tweet FROM retweet WHERE user = " + userId;
+                using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                        long tweet = (long)reader.GetValue(0);
+                        tweetList.Add(tweet);
+                    }
+                }
+            }
+            return tweetList;
+        }
+
+        public HashSet<long> getQuotedTweets(long userId) {
+            HashSet<long> tweetList = new HashSet<long>();
+            using (SQLiteCommand cmd = new SQLiteCommand(conn)) {
+                cmd.CommandText = "SELECT tweet FROM quote WHERE user = " + userId;
+                using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                        long tweet = (long)reader.GetValue(0);
+                        tweetList.Add(tweet);
+                    }
+                }
+            }
+            return tweetList;
+        }
+
+        public HashSet<long> getFavoriteTweets(long userId) {
+            HashSet<long> tweetList = new HashSet<long>();
+            using (SQLiteCommand cmd = new SQLiteCommand(conn)) {
+                cmd.CommandText = "SELECT tweet FROM favorite WHERE user = " + userId;
+                using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                        long tweet = (long)reader.GetValue(0);
+                        tweetList.Add(tweet);
+                    }
+                }
+            }
+            return tweetList;
+        }
+
+        public Dictionary<long, int> getMentionCounts(long userId) {
+            Dictionary<long, int> mentionCounts = new Dictionary<long, int>();
+            using (SQLiteCommand cmd = new SQLiteCommand(conn)) {
+                cmd.CommandText = "SELECT target FROM mention WHERE source = " + userId;
+                using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                        long target = (long)reader.GetValue(0);
+                        if (!mentionCounts.ContainsKey(target))
+                            mentionCounts.Add(target, 1);
+                        else
+                            mentionCounts[target] += 1;
+                    }
+                }
+            }
+            return mentionCounts;
+        }
+
+        public int getMentionCount(string userId1, string userId2) {
+            int mentionCount = 0;
+            using (SQLiteCommand cmd = new SQLiteCommand(conn)) {
+                cmd.CommandText = "SELECT COUNT(*) FROM mention WHERE source = " + userId1 + " AND target = " + userId2;
+                Int64 count = (Int64)cmd.ExecuteScalar();
+                mentionCount += (int)count;
+                cmd.CommandText = "SELECT COUNT(*) FROM mention WHERE source = " + userId2 + " AND target = " + userId1;
+                count = (Int64)cmd.ExecuteScalar();
+                mentionCount += (int)count;
+            }
+            return mentionCount;
+        }
+    }
+}
